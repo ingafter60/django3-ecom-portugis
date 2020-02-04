5318d8b002e85122403b0216a5e144cea0997be6
ing <inyoman_gurnitha@yahoo.com>
2020-02-04 16:21:55 +0700

    adiciona templates

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -13,20 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from .core import views 
-
-urlpatterns = [
-	 path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-]
-
-# from django.conf.urls import url
 # from django.contrib import admin
-# from .core import views
+# from django.urls import path
+# from .core import views 
 
 # urlpatterns = [
-#     url(r'^$', views.home, name='home'),
-#     url(r'^admin/', admin.site.urls),
+# 	 path('', views.home, name='home'),
+#     path('admin/', admin.site.urls),
 # ]
+
+from django.conf.urls import url
+from django.contrib import admin
+from .core import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^produto/$', views.product, name='product'),
+    url(r'^produtos/$', views.product_list, name='product_list'),
+    url(r'^admin/', admin.site.urls),
+]
