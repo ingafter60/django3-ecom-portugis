5a6f513581fdb61640bd82b27721f33af54d4715
ing <inyoman_gurnitha@yahoo.com>
2020-02-04 21:47:25 +0700

    10. Display products

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -23,13 +23,21 @@ Including another URLconf
 # ]
 
 from django.conf.urls import url
+from django.urls import include
 from django.contrib import admin
 from .core import views
 
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+#     url(r'^contato/$', views.contact, name='contact'),
+#     url(r'^produto/$', views.product, name='product'),
+#     url(r'^produtos/$', views.product_list, name='product_list'),
+#     url(r'^admin/', admin.site.urls),
+# ]
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^contato/$', views.contact, name='contact'),
-    url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', views.product_list, name='product_list'),
+    url(r'^produtos/', include('djangoecommerce.catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
